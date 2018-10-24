b2b97ebc267f7e4f9f57a645f084e9c86c85b256
Unknown <guileme13@gmail.com>
2018-10-24 06:47:33 -0300

    cap-8

--- common/build-ms.sh
+++ common/build-ms.sh
@@ -4,4 +4,5 @@ mvn -f ~/workspace/sbms-specialroutes-service/ clean package -Dmaven.test.skip d
 mvn -f ~/workspace/organization-service-new/ clean package -Dmaven.test.skip docker:build 
 mvn -f ~/workspace/sbms-organization-service/ clean package -Dmaven.test.skip docker:build 
 mvn -f ~/workspace/sbms-eurekasr/ clean package -Dmaven.test.skip docker:build 
-mvn -f ~/workspace/sbms-confsvr/ clean package -Dmaven.test.skip docker:build 
+mvn -f ~/workspace/sbms-confsvr/ clean package -Dmaven.test.skip docker:build
+mvn -f ~/workspace/sbms-licensing-service/ clean package -Dmaven.test.skip docker:build
