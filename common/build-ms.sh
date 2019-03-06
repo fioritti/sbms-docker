e1350b7b2212401a42ba16a14ee68269f8f23034
Unknown <guileme13@gmail.com>
2019-03-06 20:32:13 -0300

    cap-9

--- common/build-ms.sh
+++ common/build-ms.sh
@@ -1,8 +1,7 @@
-mvn -f ~/workspace/sbms-authentication-service/ clean package -Dmaven.test.skip docker:build 
+mvn -f /media/guilherme/nova_parti/workspace/sbms/sbms-authentication-service clean package -Dmaven.test.skip docker:build 
 mvn -f ~/workspace/sbms-zuul-server/ clean package -Dmaven.test.skip docker:build 
-mvn -f ~/workspace/sbms-specialroutes-service/ clean package -Dmaven.test.skip docker:build 
-mvn -f ~/workspace/organization-service-new/ clean package -Dmaven.test.skip docker:build 
 mvn -f ~/workspace/sbms-organization-service/ clean package -Dmaven.test.skip docker:build 
-mvn -f ~/workspace/sbms-eurekasr/ clean package -Dmaven.test.skip docker:build 
+mvn -f /media/guilherme/nova_parti/workspace/sbms/sbms-eurekasr clean package -Dmaven.test.skip docker:build 
 mvn -f ~/workspace/sbms-confsvr/ clean package -Dmaven.test.skip docker:build
 mvn -f ~/workspace/sbms-licensing-service/ clean package -Dmaven.test.skip docker:build
+mvn -f ~/workspace/sbms-zipkin-server/ clean package -Dmaven.test.skip docker:build
